c0eecc07c9d7eea7175105a20c4f1f65418e5a87
Michael Gusev <m@sudo.eu>
2020-03-26 10:12:04 +0100

    Init

---
+++ README.md
@@ -0,0 +1,17 @@
+# Requirements
+* You can change the code only inside of the `.pipe` in the `AppComponent`.
+
+# Goal
+
+* By default the counter doesn't count and its value is 0
+* The counting begins when we click the start button, 1 2 3 etc.
+* The counting stops when we click the stop button, (for example on 20).
+* The counting continues when we click the start button again, 21 22 23 etc.
+
+# Extra (not required)
+
+* A click on the start button after a click on the stop button should start from 0.
+
+# Send result
+
+Send me a message / an email with a code I need to put into the `.pipe`.
---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import {ChangeDetectionStrategy, Component, EventEmitter} from '@angular/core';
+import {interval, Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AppComponent {
+  public readonly data$: Observable<{
+    counter: number,
+  }>;
+
+  public readonly start$: EventEmitter<void> = new EventEmitter();
+  public readonly stop$: EventEmitter<void> = new EventEmitter();
+
+  constructor() {
+    this.data$ = interval(500).pipe(
+      // Put your code here. Feel free to change the lines below too.
+
+      map(counter => counter + 1),
+      startWith(0),
+      map(counter => ({counter})),
+    );
+  }
+}
